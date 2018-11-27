9e41d4f5093adcf8d60782d30184b7fbecd3b063
qq.ping <ping.qq@ybm100.com>
2018-11-27 18:21:00 +0800

    change

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from login import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^index/', views.index),
+    url(r'^login/', views.login),
+    url(r'^register/', views.register),
+    url(r'^logout/', views.logout),
 ]
